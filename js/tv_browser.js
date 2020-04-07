3ec23b5993b62174a6f2ab5d2e9aacfe8d95e1e0
Anil Mirge <manilche@gmail.com>
2020-04-07 13:40:24 -0400

    Enhancing the code a bit to handle clearing fields on new search

--- js/tv_browser.js
+++ js/tv_browser.js
@@ -6,6 +6,8 @@ form.addEventListener('submit', function (event) {
 	let showSelect = document.getElementById('show-select');
 	showSelect.style.display = 'block';
 	let title = document.getElementById('show-search').value;
+	let showDetail = document.getElementById('show-detail');
+	showDetail.innerHTML = '';
 	const url = `http://api.tvmaze.com/search/shows?q=${title}`;
 
 	fetch(url)
@@ -15,20 +17,14 @@ form.addEventListener('submit', function (event) {
 		.then((res) => {
 			console.log('success!', res);
 			if (res && res.length > 0) {
-				showSelect.removeChild(showSelect.options[0]); // this is to remove the original 'Select a show...' option from select
-				let firstOption = document.createElement('option');
-				firstOption.appendChild(
-					document.createTextNode(`Shows matching ${title}...`)
-				);
-				firstOption.value = '';
-				showSelect.appendChild(firstOption);
+				let selectOptions = `<option value=''>Shows matching ${title}...</option>`;
 				for (let i = 0; i < res.length; i++) {
 					let currentRow = res[i];
-					let option = document.createElement('option');
-					option.appendChild(document.createTextNode(currentRow.show.name));
-					option.value = currentRow.show.id;
-					showSelect.appendChild(option);
+					selectOptions =
+						selectOptions +
+						`<option value="${currentRow.show.id}">${currentRow.show.name}</option>`;
 				}
+				showSelect.innerHTML = selectOptions;
 			}
 		})
 		.catch((err) => {
@@ -37,7 +33,6 @@ form.addEventListener('submit', function (event) {
 
 	showSelect.addEventListener('change', function (event) {
 		event.preventDefault();
-		let showDetail = document.getElementById('show-detail');
 		showDetail.innerHTML = '';
 		let showId = event.target.value;
 		const showUrl = `http://api.tvmaze.com/shows/${showId}`;
@@ -48,17 +43,17 @@ form.addEventListener('submit', function (event) {
 			.then((res) => {
 				console.log('success!', res);
 				if (res) {
-					let h2 = `<h2>${res.name}</h2>`
+					let h2 = `<h2>${res.name}</h2>`;
 					let image = '';
 					if (res.image) {
 						image = `<img src="${res.image.original}" alt="${res.name}"></img>`;
-                    } 
-                    let summary = `<p>Summary not available</p>`;
+					}
+					let summary = `<p>Summary not available</p>`;
 					if (res.summary) {
 						summary = res.summary;
-                    }
-                    let detail = h2 + image + summary;
-                    showDetail.innerHTML = detail;
+					}
+					let detail = h2 + image + summary;
+					showDetail.innerHTML = detail;
 				}
 			})
 			.catch((err) => {
